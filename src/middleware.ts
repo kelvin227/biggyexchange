44f332eb397cf8bcf07522d0bf03b63112f753a3
kelvin ngene <162456111+kelvin227@users.noreply.github.com>
2025-07-24 22:19:09 +0100

    made changes to the middleware used for development

--- src/middleware.ts
+++ src/middleware.ts
@@ -29,8 +29,8 @@ export default auth(async (req) => {
     switch (subdomain) {
 
         ///change the case back to app later when he has bought a domain
-        //case "biggyexchange":
-        case "app":
+        case "biggyexchange":
+        //case "app":
             {
                 const PROTECTED_PATHS = ["/user_dashboard", "/wallet", "/otc", "/profile", "/profile/overview", "/profile/security", "/profile/user_kyc", "/profile/task", "/wallet/payment/crypto", "/wallet/transaction", "/otc", "/otc/advertisement", "/otc/advertisement/atok", "/otc/advertisement/opin", "/otc/advertisement/rbl", "/otc/advertisement/sda", "/otc/advertisement/star", "/otc/advertisement/wow"]; // Add all your exact protected paths here
                 const isProtectedPath = PROTECTED_PATHS.includes(pathname);
@@ -43,8 +43,8 @@ export default auth(async (req) => {
             }
 
             /// chnage the case back to admin or web when he has bought a domain
-        //case "adminBiggyexchange":
-        case "web":
+        case "adminBiggyexchange":
+        //case "web":
             {
                 const PROTECTED_PATHS = ["/", "/profile", "/settings", "/checkout", "/orders"]; // Add all your exact protected paths here
                 const isProtectedPath = PROTECTED_PATHS.includes(pathname);
